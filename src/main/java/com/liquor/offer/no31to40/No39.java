4b0036d249a1e3db1182325e8ffd1d7e526caf04
zhengzhicong <1064275075@qq.com>
2020-01-02 20:39:41 +0800

    排序数组中一个数字出现的次数

---
+++ src/main/java/com/liquor/offer/no31to40/No39.java
@@ -0,0 +1,18 @@
+package com.liquor.offer.no31to40;
+
+/**
+ * 统计一个数字在排序数组中出现的次数。
+ *
+ * 排序的数组，用二分查找
+ */
+public class No39 {
+
+    public int GetNumberOfK(int [] array , int k) {
+
+        if (array.length == 0){
+            return -1;
+        }
+
+        return 0;
+    }
+}
