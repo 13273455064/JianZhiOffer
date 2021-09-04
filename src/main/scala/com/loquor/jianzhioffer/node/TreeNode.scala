d7463e92b74d51e948d5d1183416e7505addf529
zzc <1064275075@qq.com>
2021-09-04 13:27:32 +0800

    scala: add TreeNode.scala

---
+++ src/main/scala/com/loquor/jianzhioffer/node/TreeNode.scala
@@ -0,0 +1,11 @@
+package com.loquor.jianzhioffer.node
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author zhengzhicong
+ * @since 2021/8/8 3:48 下午
+ */
+case class TreeNode(value: Int,var left: TreeNode,var right:TreeNode)
