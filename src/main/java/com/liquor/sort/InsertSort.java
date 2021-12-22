5fa377b1d3f97cc985d1c6fe96d7ee6d00f5ff4d
zhengzhicong <zhengzhicong@growingio.com>
2021-12-22 21:38:58 +0800

    插入排序

---
+++ src/main/java/com/liquor/sort/InsertSort.java
@@ -0,0 +1,23 @@
+package com.liquor.sort;
+
+import java.util.Arrays;
+
+public class InsertSort {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,3,1,5,8,32,4,100,7,6,11};
+        final int length = arr.length;
+
+        for (int i = 1; i < length; i++) {
+
+            int temp = arr[i];
+
+            for (int j = i ; j > 0 && arr[j - 1] > arr[j]; j--) {
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
