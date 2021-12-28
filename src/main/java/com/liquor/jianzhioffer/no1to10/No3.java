066bdb7b1be6083d012ec0bc74c013a73f8791ce
zhengzhicong <zhengzhicong@growingio.com>
2021-12-28 22:55:40 +0800

    从尾到头打印链表，翻转链表写法

--- src/main/java/com/liquor/jianzhioffer/no1to10/No3.java
+++ src/main/java/com/liquor/jianzhioffer/no1to10/No3.java
@@ -26,34 +26,55 @@ public class No3 {
         ln8.next = null;
 
         printListFromTailToHead(ln1);
+        print(ln1);
         String s;
     }
 
     public static ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
 
         ArrayList<Integer> result = new ArrayList<Integer>();
-        if (listNode==null){
+        if (listNode == null) {
             return result;
         }
         Stack<ListNode> stack = new Stack<ListNode>();
-        while (listNode!=null){
+        while (listNode != null) {
             stack.push(listNode);
             listNode = listNode.next;
         }
-        while (!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             result.add(stack.pop().val);
+        }
+        return result;
+    }
+
+    static int[] print(ListNode head) {
+        ListNode pre = null;
+        ListNode current = head;
+
+        int size = 0;
+        while(current != null) {
+            ListNode temp = current.next;
+            current.next = pre;
+            pre = current;
+            current = temp;
+            size++;
+        }
+        int[] result = new int[size];
+        for(int i = 0;i<size;i++) {
+            result[i] = pre.val;
+            pre = pre.next;
         }
         return result;
     }
 
 
     //自定义的链表内部类
-    static class ListNode{
+    static class ListNode {
         int val;
         //指向下一个节点的指针
         ListNode next = null;
 
-        ListNode(int val){
+        ListNode(int val) {
             this.val = val;
         }
 
