4469f811258bef66775712dd478526605370c926
zhengzhicong <zhengzhicong@mokahr.com>
2022-05-11 11:34:31 +0800

    update TreeNode class and add No124

---
+++ src/main/scala/com/loquor/jianzhioffer/No124.scala
@@ -0,0 +1,30 @@
+package com.loquor.jianzhioffer
+
+import com.loquor.jianzhioffer.node.TreeNode
+
+object No124 {
+
+
+  def main(args: Array[String]): Unit = {
+    var maxP: Int = Int.MinValue
+
+    def maxPathSum(root: TreeNode): Int = {
+      Option.apply(root) match {
+        case None => 0
+        case Some(_) =>
+          val leftMax = math.max(maxPathSum(root.left), 0)
+          val rightMax = math.max(maxPathSum(root.right), 0)
+          maxP = math.max(leftMax + rightMax + root.value, maxP)
+          math.max(leftMax, rightMax) + root.value
+      }
+    }
+
+    val root = TreeNode.getInstance()
+    maxPathSum(root)
+    println(maxP)
+
+  }
+
+
+
+}
--- src/main/scala/com/loquor/jianzhioffer/node/TreeNode.scala
+++ src/main/scala/com/loquor/jianzhioffer/node/TreeNode.scala
@@ -8,4 +8,30 @@ package com.loquor.jianzhioffer.node
  * @author zhengzhicong
  * @since 2021/8/8 3:48 下午
  */
-case class TreeNode(value: Int,var left: TreeNode,var right:TreeNode)
+case class TreeNode(value: Int, var left: TreeNode = null, var right: TreeNode = null) {
+
+}
+
+object TreeNode {
+
+  /**
+   * [-10,9,20,null,null,15,7]
+   *
+   *     -10
+   *   9     20
+   *       15   7
+   */
+  def getInstance(): TreeNode = {
+    val node_10 = TreeNode(-10)
+    val node9 = TreeNode(9)
+    val node20 = TreeNode(20)
+    val node15 = TreeNode(15)
+    val node7 = TreeNode(7)
+
+    node_10.left = node9
+    node_10.right = node20
+    node20.left = node15
+    node20.right = node7
+    node_10
+  }
+}
