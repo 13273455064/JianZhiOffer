ddf87c1e26fb9161ff4d21e824d91f81db79f191
zhengzhicong <zhengzhicong>
2023-08-11 15:19:40 +0800

    update

--- src/main/java/com/liquor/leetcode/Tree.java
+++ src/main/java/com/liquor/leetcode/Tree.java
@@ -10,11 +10,6 @@ public class Tree {
         int val = 0;
         TreeNode left = null;
         TreeNode right = null;
-
-        public TreeNode(int val) {
-            this.val = val;
-
-        }
     }
 
     public static void main(String[] args) {
